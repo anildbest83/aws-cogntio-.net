41dfef95305f449b39348bde1d25fde212c313aa
anildbest83 <anildbest83@gmail.com>
2018-05-01 16:17:27 +0530

    Add files via upload

---
+++ RequestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+
+namespace CognitoApi
+{
+    public class RequestResult
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public string Token { get; set; }
+        public object Data { get; set; }
+        public string Id { get; set; }
+        public IEnumerable<Claim> cc { get; set; }
+    }
+}
